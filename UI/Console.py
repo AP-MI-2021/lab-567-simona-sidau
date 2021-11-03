fe1d17e680e2d070c01a91e31a3d9952621c060e
Simona Sidau <simona.sidau09@gmail.com>
2021-11-03 08:30:37 +0200

    lab6

--- UI/Console.py
+++ UI/Console.py
@@ -1,5 +1,5 @@
 from Domain.librarie import to_string
-from Logic.CRUD import adaugare_vanzare, sterge_vanzare, modifica_vanzare
+from Logic.CRUD import adaugare_vanzare, sterge_vanzare, modifica_vanzare, get_by_id
 from Logic.functionalitati import reducere_pret, modificare_gen_pt_titlu_dat, pret_minim_per_gen, ordonare_dupa_pret, \
     numar_titluri_distincte_per_gen
 
@@ -24,6 +24,8 @@ def ui_adaugare_vanzare(lista):
         gen = input("Dati genul cartii ")
         pret = float(input("Dati pretul cartii "))
         reducere = input("Dati tipul de reducere ")
+        if get_by_id(id, lista) is not None:
+            raise ValueError("Id-ul exista deja!")
         return adaugare_vanzare(id, titlu, gen, pret, reducere, lista)
     except ValueError as ve:
         print("Eroare {}".format(ve))
@@ -79,30 +81,72 @@ def ui_numar_titluri_distincte_per_gen(lista):
         print("Genul {} are {} titluri distincte".format(gen, rezultat[gen]))
 
 
+def print_console_menu():
+    print("i. Consola veche")
+    print("ii. Command line console ")
+    print("x.Exit")
+
+
+def print_comand_line_console_menu():
+    print("1. Adaugare vanzare")
+    print("2. Sterge vanzare")
+    print("3. Show all")
+    print("x. Exit")
+
+
 def run_menu(lista):
     while True:
-        print_menu()
-        optiune = input("Dati optiunea: ")
+        print_console_menu()
+        optiune = input("Alegeti consola pe care o doriti: ")
+        if optiune == "i":
+            while True:
+                print_menu()
+                optiune = input("Dati optiunea: ")
 
-        if optiune == "1":
-            lista = ui_adaugare_vanzare(lista)
-        elif optiune == "2":
-            lista = ui_sterge_vanzare(lista)
-        elif optiune == "3":
-            lista = ui_mofificare_vanzare(lista)
-        elif optiune == "4":
-            lista = ui_reducere_pret(lista)
-        elif optiune == "5":
-            lista = ui_modificare_gen_pt_titlu_dat(lista)
-        elif optiune == "6":
-            ui_pret_minim_per_gen(lista)
-        elif optiune == "7":
-            ui_ordonare_dupa_pret(lista)
-        elif optiune == "8":
-            ui_numar_titluri_distincte_per_gen(lista)
-        elif optiune == "a":
-            afisare(lista)
+                if optiune == "1":
+                    lista = ui_adaugare_vanzare(lista)
+                elif optiune == "2":
+                    lista = ui_sterge_vanzare(lista)
+                elif optiune == "3":
+                    lista = ui_mofificare_vanzare(lista)
+                elif optiune == "4":
+                    lista = ui_reducere_pret(lista)
+                elif optiune == "5":
+                    lista = ui_modificare_gen_pt_titlu_dat(lista)
+                elif optiune == "6":
+                    ui_pret_minim_per_gen(lista)
+                elif optiune == "7":
+                    ui_ordonare_dupa_pret(lista)
+                elif optiune == "8":
+                    ui_numar_titluri_distincte_per_gen(lista)
+                elif optiune == "a":
+                    afisare(lista)
+                elif optiune == "x":
+                    break
+                else:
+                    print("Optiune inexistenta! Reincercati!")
+        elif optiune == "ii":
+            should_run = True
+            while should_run:
+                print_comand_line_console_menu()
+                linie_optiune = input("Introduceti comenzile separete prin virgula: ")
+                ajutor_optiune = linie_optiune.split(",")
+                for optiune in ajutor_optiune:
+                    if optiune == "1":
+                        lista = ui_adaugare_vanzare(lista)
+                        print("Optiunea 1 realizata cu succes!")
+                    elif optiune == "2":
+                        lista = ui_sterge_vanzare(lista)
+                        print("Optiunea 2 realizata cu succes!")
+                    elif optiune == "3":
+                        afisare(lista)
+                        print("Optiunea 3 realizata cu succes!")
+                    elif optiune == "x":
+                        should_run = False
+                        break
+                    else:
+                        print("Optiunea {} este inexistenta! Reincercati".format(optiune))
         elif optiune == "x":
             break
         else:
-            print("Optiune inexistenta! Reincercati!")
+            print("Optiune inexistenta. Reincercati!")
